236ee329e91c0b230c7030bac05df1fd8954bd9a
Ruslan <ruslan_gaiazov@epam.com>
2020-05-15 14:38:21 +0300

    Implement ToastModule for android and ios

--- src/screens/main/main.js
+++ src/screens/main/main.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { ScrollView } from 'react-native';
 
 import { ProductsList, CategoriesList } from './components';
-import { Divider } from 'shared/components';
+import { Divider, Button } from 'shared/components';
+import ToastModule from 'react-native-toast-module';
 
 export const Main = ({ navigation }) => {
+    const onPressShowToast = () => {
+        ToastModule.show('Toast here!!!');
+    };
     return (
         <ScrollView>
+            <Button title="Show Toast" onPress={onPressShowToast} />
             <CategoriesList />
             <Divider />
             <ProductsList navigation={navigation} />
