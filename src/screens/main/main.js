df3bc3295886ce836980fbf7fe526526ff9681f9
Ruslan <ruslan_gaiazov@epam.com>
2020-05-16 15:18:55 +0300

    Move ToastModule to ProductDetails

--- src/screens/main/main.js
+++ src/screens/main/main.js
@@ -2,16 +2,11 @@ import React from 'react';
 import { ScrollView } from 'react-native';
 
 import { ProductsList, CategoriesList } from './components';
-import { Divider, Button } from 'shared/components';
-import ToastModule from 'react-native-toast-module';
+import { Divider } from 'shared/components';
 
 export const Main = ({ navigation }) => {
-    const onPressShowToast = () => {
-        ToastModule.show('Toast here!!!');
-    };
     return (
         <ScrollView>
-            <Button title="Show Toast" onPress={onPressShowToast} />
             <CategoriesList />
             <Divider />
             <ProductsList navigation={navigation} />
