45dc603513643c1ba8a0f9b7f8f765a053e0d4a2
Ruslan <ruslan_gaiazov@epam.com>
2020-05-19 21:59:43 +0300

    Add some tests

--- __tests__/screens/login/login.test.js
+++ __tests__/screens/login/login.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Alert } from 'react-native';
 import { shallow } from 'enzyme';
+import * as redux from 'react-redux';
 
 import { LoginScreen } from 'screens';
+import { loc } from 'shared/assets';
+import { routes } from 'shared/constants';
 
 const mockNavigation = {
     navigate: jest.fn(),
 };
+
+jest.spyOn(redux, 'useDispatch').mockImplementation(jest.fn());
+jest.spyOn(Alert, 'alert').mockImplementation(jest.fn());
 
 describe('Login screen', () => {
     let wrapper;
@@ -16,5 +23,15 @@ describe('Login screen', () => {
 
     it('should render fine', () => {
         expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should show alert message', () => {
+        wrapper.find({ title: loc('login.navQuestion1.title') }).simulate('press');
+        expect(Alert.alert).toHaveBeenCalledWith(loc('login.navQuestion1.move'));
+    });
+
+    it('should navigate to registration screen', () => {
+        wrapper.find({ title: loc('login.navQuestion2.title') }).simulate('press');
+        expect(mockNavigation.navigate).toHaveBeenCalledWith(routes.REGISTRATION);
     });
 });
